85e5f6379c55e30907696f42ec8df3a8ab7df0a3
edsom <edsonfreitas499@gmail.com>
2019-12-09 20:18:00 -0300

    pasta log_res

---
+++ logica_e_responsivo/ling.c
@@ -0,0 +1,28 @@
+#include<stdio.h>  
+ main()  
+ {  
+   int valor,total=0,cont;  
+   for(cont=1;cont<=30;cont++)  
+   {  
+    printf("Digite o %d\xa7 Valor:\t ",cont);  
+    scanf("%d",&valor);  
+    system("cls");  
+    total=total+valor;  
+   }  
+   printf("Total: %d",total);  
+   getch();  
+ }  
+ #include<stdio.h>  
+ main()  
+ {  
+   int valor,total=0,cont;  
+   for(cont=1;cont<=30;cont++)  
+   {  
+    printf("Digite o %d\xa7 Valor:\t ",cont);  
+    scanf("%d",&valor);  
+    system("cls");  
+    total=total+valor;  
+   }  
+   printf("Total: %d",total);  
+   getch();  
+ }  
